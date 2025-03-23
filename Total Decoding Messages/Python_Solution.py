9d271f23e86ba78ce1e0b69d5ef6209e5720f2cd
Kaushik Das <kudokaito.pd@gmail.com>
2025-03-23 09:28:24 +0530

    Create Python_Solution.py

---
+++ Total Decoding Messages/Python_Solution.py
@@ -0,0 +1,20 @@
+class Python_Solution:
+	def countWays(self, digits):
+		# Code here
+		if not digits or digits[0] == '0':
+            return 0
+    
+        n = len(digits)
+        dp = [0] * (n + 1)
+        dp[0], dp[1] = 1, 1
+    
+        for i in range(2, n + 1):
+            one_digit = int(digits[i - 1])
+            two_digits = int(digits[i - 2:i])
+    
+            if 1 <= one_digit:
+                dp[i] += dp[i - 1]
+            if 10 <= two_digits <= 26:
+                dp[i] += dp[i - 2]
+    
+        return dp[n]
